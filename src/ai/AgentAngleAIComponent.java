7250403437616175bd99090faeaf7a720ace5ec6
Raghav Narula <ragnarula87@googlemail.com>
2015-11-06 16:05:42 +0000

    some playing around with vectors

---
+++ src/ai/AgentAngleAIComponent.java
@@ -0,0 +1,13 @@
+package ai;
+
+import ai.model.EnvironmentModel;
+
+/**
+ * Created by raghavnarula on 06/11/2015.
+ */
+public class AgentAngleAIComponent extends AbstractSimpleAIComponent {
+    @Override
+    EnvironmentModel processModel(EnvironmentModel model) {
+        return null;
+    }
+}
