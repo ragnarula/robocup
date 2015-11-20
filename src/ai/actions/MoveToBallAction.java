474fda6866825233db8e27f75524404a1d70da74
James Frost <hello@jamesfrost.me>
2015-11-20 19:03:48 +0000

    MoveToBall action now moves to where the ball is going

--- src/ai/actions/MoveToBallAction.java
+++ src/ai/actions/MoveToBallAction.java
@@ -2,6 +2,7 @@ package ai.actions;
 
 import ai.model.CommandPlayer;
 import ai.model.EnvironmentModel;
+import info.SeeBallInfo;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 /**
@@ -16,7 +17,25 @@ public class MoveToBallAction implements Action {
         Vector2D ownGoalLocation = model.getOwnGoalLocation();
         double angleToTurn = model.getBallAngle() - model.getAgentAbsAngleRadians();
 
-        if ( Math.abs(angleToTurn) < (Math.PI / 4) ) {
+        if (!model.getLastPercept().getSeenBalls().isEmpty()) {
+            SeeBallInfo seeBallInfo = model.getLastPercept().getLastSeenBall();
+            double dirChange = seeBallInfo.getDirChange();
+            double distanceToBall = seeBallInfo.getDistance();
+
+            if (distanceToBall > 5) {
+
+                angleToTurn += dirChange;
+                
+                if (Math.abs(angleToTurn) > (Math.PI / 3)) {
+                    if (angleToTurn > 0)
+                        angleToTurn = (Math.PI / 3);
+                    else
+                        angleToTurn = -(Math.PI / 3);
+                }
+            }
+        }
+
+        if (Math.abs(angleToTurn) <= (Math.PI / 3)) {
             if (ballLocation.distance(ownGoalLocation) < 23.5 || ballLocation.distance(goalLocation) < 23.5) {
                 player.dash(120);
             } else {
