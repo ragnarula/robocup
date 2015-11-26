4d18523f37d0c5a34bf5b3e2ff46d80e3f13d3ff
Raghav Narula <ragnarula87@googlemail.com>
2015-11-26 17:14:47 +0000

    jar file

--- src/Run.java
+++ src/Run.java
@@ -18,7 +18,7 @@ public class Run {
         AbstractTeam team = null;
         if (args.length == 0) {
             //using our agent.Team
-            team = new Team("IAFC2", 6000, "localhost", true);
+            team = new Team("IAFC", 6000, "localhost", true);
         } else {
             Integer val      = new Integer(args[0]);
             int     port     = val.intValue();
